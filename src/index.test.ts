213071cf32e5087b77ddf3a68d20f892c6e5303d
shixiongfei <xiongfei.shi@icloud.com>
2024-04-14 15:17:06 +0800

    Finish NanoRPC http client

--- src/index.test.ts
+++ src/index.test.ts
@@ -13,15 +13,20 @@ import { createNanoRPCClient } from "./index.js";
 import { signature as signatureNode } from "./hashes.js";
 import { signature as signatureBrowser } from "./hashes.browser.js";
 
+type AddRPCFunc = (a: number, b: number) => Promise<number | undefined>;
+
 const test = async () => {
-  const secret = "52440ec2-2a22-4544-93a7-161dfc47239a";
+  console.log(signatureNode("1234567890", "HelloWorld!"));
+  console.log(signatureBrowser("1234567890", "HelloWorld!"));
 
-  const rpc = createNanoRPCClient("http://127.0.0.1:4000", secret);
+  const rpc = createNanoRPCClient(
+    "http://127.0.0.1:4000",
+    "52440ec2-2a22-4544-93a7-161dfc47239a",
+  );
 
-  console.log(signatureNode(secret, "HelloWorld!"));
-  console.log(signatureBrowser(secret, "HelloWorld!"));
+  const addRPC: AddRPCFunc = rpc.invoke("add");
 
-  console.log(await rpc.apply("add", [2, 3]));
+  console.log(await Promise.all([addRPC(23, 31), rpc.call("add", 123, 456)]));
 };
 
 test();
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,24 @@
  * https://github.com/shixiongfei/nanorpc-http-client
  */
 
-import { NanoValidator, createNanoValidator } from "nanorpc-validator";
+import * as R from "ramda";
+import {
+  NanoReply,
+  NanoValidator,
+  createNanoRPC,
+  createNanoValidator,
+} from "nanorpc-validator";
+import { signature } from "./hashes.js";
+
+export enum NanoRPCCode {
+  OK = 0,
+}
+
+type NanoRPCHttpResp<T> = {
+  code: number;
+  data?: NanoReply<T>;
+  error?: { name: string; message: string };
+};
 
 export class NanoRPCClient {
   public readonly validators: NanoValidator;
@@ -26,7 +43,58 @@ export class NanoRPCClient {
     method: M,
     args: P,
   ) {
-    return new URL(`/nanorpcs/${method}`, this.api);
+    const rpc = createNanoRPC(method, args);
+    const payload = R.join(
+      "\n",
+      R.sort(
+        R.comparator((a, b) => a.localeCompare(b) < 0),
+        R.map(([key, value]) => `${key.toString()}=${value}`, R.toPairs(rpc)),
+      ),
+    );
+    const sign = signature(this.secret, payload);
+
+    const content = await fetch(new URL(`/nanorpcs/${method}`, this.api), {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify({ ...rpc, sign }),
+    });
+
+    const json: NanoRPCHttpResp<T> = await content.json();
+
+    if (!R.has("code", json)) {
+      throw new Error(`NanoRPC call ${method} received reply missing code`);
+    }
+
+    if (json.code !== 200) {
+      if (R.has("error", json)) {
+        const error = `${json.code} ${json.error!.name}: ${json.error!.message}`;
+        throw new Error(`NanoRPC call ${method}, ${error}`);
+      }
+
+      throw new Error(`NanoRPC call ${method}, unknown error`);
+    }
+
+    if (!R.has("data", json)) {
+      throw new Error(`NanoRPC call ${method} received reply missing data`);
+    }
+
+    const reply = json.data!;
+    const validator = this.validators.getValidator(method);
+
+    if (validator && !validator(reply)) {
+      const lines = validator.errors!.map(
+        (err) => `${err.keyword}: ${err.instancePath}, ${err.message}`,
+      );
+      const error = lines.join("\n");
+
+      throw new Error(`NanoRPC call ${method}, ${error}`);
+    }
+
+    if (reply.code !== NanoRPCCode.OK) {
+      throw new Error(`NanoRPC call ${method} ${reply.message}`);
+    }
+
+    return reply.value;
   }
 
   async call<T, M extends string, P extends Array<unknown>>(
